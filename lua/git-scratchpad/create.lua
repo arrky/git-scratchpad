8519eb410b805e0fc4bb92a8b1ceadb317850570
arrky <81797775+arrky@users.noreply.github.com>
2023-10-13 23:41:24 -0300

    add .scratchpad to git exclude during creation

--- lua/git-scratchpad/create.lua
+++ lua/git-scratchpad/create.lua
@@ -12,6 +12,19 @@ git_toplevel = vim.fn.fnamemodify(git_toplevel, ":p")
 local scratchpad_dir = git_toplevel .. ".scratchpad"
 
 
+local function gitExcludeScratchpad()
+  local git_exclude_path = absolute_git_dir .. "info/exclude"
+
+  local exclude_file = io.open(git_exclude_path, "a")
+
+  if exclude_file then
+    exclude_file:write(".scratchpad/*")
+    exclude_file:close()
+    notify("scratchpad added to .git/info/exclude!")
+  end
+end
+
+
 function M.new_note()
   local note_path = scratchpad_dir .. "/scratch.md"
 
@@ -24,6 +37,8 @@ function M.new_note()
   if vim.fn.isdirectory(scratchpad_dir) == 0 then
     vim.fn.mkdir(scratchpad_dir, "p")
     notify("scratchpad directory created!")
+
+    gitExcludeScratchpad()
   end
 
   local scratch_file = io.open(note_path, "a")
