b705068ce54eb29a6765150b61469d4d05d894c5
arrky <81797775+arrky@users.noreply.github.com>
2023-10-16 10:53:24 -0300

    add messages for cloning plenary.nvim

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,7 +1,12 @@
 local plenary_dir = os.getenv("PLENARY_DIR") or "/tmp/plenary.nvim"
 local no_plenary_dir = vim.fn.isdirectory(plenary_dir) == 0
+
 if no_plenary_dir then
+  print("Cloning plenary.nvim at " .. plenary_dir .. "...\n")
+
   vim.fn.system({ "git", "clone", "https://github.com/nvim-lua/plenary.nvim", plenary_dir })
+
+  print("Done.\n")
 end
 
 vim.opt.rtp:append(".")
