125d2be3351f45ffbea54ae956cc574db7dd78cb
arrky <81797775+arrky@users.noreply.github.com>
2023-10-15 23:33:48 -0300

    add tests' init

---
+++ tests/minimal_init.lua
@@ -0,0 +1,11 @@
+local plenary_dir = os.getenv("PLENARY_DIR") or "/tmp/plenary.nvim"
+local no_plenary_dir = vim.fn.isdirectory(plenary_dir) == 0
+if no_plenary_dir then
+  vim.fn.system({ "git", "clone", "https://github.com/nvim-lua/plenary.nvim", plenary_dir })
+end
+
+vim.opt.rtp:append(".")
+vim.opt.rtp:append(plenary_dir)
+
+vim.cmd("runtime plugin/plenary.vim")
+require("plenary.busted")
